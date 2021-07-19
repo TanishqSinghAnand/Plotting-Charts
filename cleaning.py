fbe365abea2b8291f9c986eb8b7150f066f8f09e
Tanishq Singh Anand <anandtanishqs@gmail.com>
2021-07-20 00:44:10 +0530

    Fiex bugs

--- cleaning.py
+++ cleaning.py
@@ -1,8 +1,17 @@
+  
 import pandas as pd
+import csv
+df = pd.read_csv('total_stars.csv')
+print(df.head())
 
-df = pd.read_csv("2.csv")
-print(df.columns)
-print(df.shape)
-del df["Unnamed: 0"]
-print(df.shape)
-df.to_csv("main.csv")
+
+df.drop(["Unnamed: 0"],axis=1,inplace=True)
+df.drop(["Unnamed: 5"],axis=1,inplace=True)
+df.drop(["Star_name"], axis=1, inplace=True)
+df.drop(["Distance"], axis=1, inplace=True)
+df.drop(["Mass"], axis=1, inplace=True)
+df.drop(["Radius"], axis=1, inplace=True)
+print(df.head())
+
+
+df.to_csv('Final.csv')
